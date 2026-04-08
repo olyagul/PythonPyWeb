cc06ffe2a334718b319432a9353963affb7a9fb3
olyagul <eternal_olya.gul@mail.ru>
2026-04-08 21:37:14 +0300

    Лаба3

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt  # Чтобы post, put, patch, delete не требовали csrf токена (небезопасно)
+from apps.db_train_alternative.models import Author
+from .serializers import AuthorModelSerializer
 
-# Create your views here.
+
+class AuthorAPIView(APIView):
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get(self, request, pk=None):
+        if pk is not None:
+            try:
+                author = Author.objects.get(pk=pk)
+                serializer = AuthorModelSerializer(author)
+                return Response(serializer.data)
+            except Author.DoesNotExist:
+                return Response({"message": "Автор не найден"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            authors = Author.objects.all()
+            serializer = AuthorSerializer(authors, many=True)
+            return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AuthorModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            return Response({"message": "Автор не найден"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = AuthorModelSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            return Response({"message": "Автор не найден"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = AuthorModelSerializer(author, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            return Response({"message": "Автор не найден"}, status=status.HTTP_404_NOT_FOUND)
+
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
