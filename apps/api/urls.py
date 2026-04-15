6813c072e76fcbc34ab68ebcb18071aaffbf4211
olyagul <eternal_olya.gul@mail.ru>
2026-04-15 19:43:27 +0300

    Laba4

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from .views import AuthorAPIView
+from .views import AuthorGenericAPIView
+from django.urls import include
+from .views import AuthorViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'authors_viewset', AuthorViewSet, basename='authors-viewset')
+
 
 urlpatterns = [
     path('authors/', AuthorAPIView.as_view(), name='author-list'),
     path('authors/<int:pk>/', AuthorAPIView.as_view(), name='author-detail'),
+    path('authors_generic/', AuthorGenericAPIView.as_view(), name='author-generic-list'),
+    path('authors_generic/<int:pk>/', AuthorGenericAPIView.as_view(), name='author-generic-detail'),
+    path('', include(router.urls)),
 ]
 
--- apps/api/views.py
+++ apps/api/views.py
@@ -3,7 +3,16 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt  # Чтобы post, put, patch, delete не требовали csrf токена (небезопасно)
 from apps.db_train_alternative.models import Author
+from django.http import Http404
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 from .serializers import AuthorModelSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class AuthorAPIView(APIView):
@@ -63,3 +72,54 @@ class AuthorAPIView(APIView):
 
         author.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AuthorGenericAPIView(GenericAPIView, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin,
+                           DestroyModelMixin):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
+
+    def get(self, request, *args, **kwargs):
+        if kwargs.get(self.lookup_field):
+            try:
+                # возвращаем один объект
+                return self.retrieve(request, *args, **kwargs)
+            except Http404:
+                return Response({'message': 'Автор не найден'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            # Иначе возвращаем список объектов
+            return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
+class AuthorPagination(PageNumberPagination):
+    page_size = 5  # количество объектов на странице
+    page_size_query_param = 'page_size'  # параметр запроса для настройки количества объектов на странице
+    max_page_size = 1000  # максимальное количество объектов на странице
+
+
+class AuthorViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
+    pagination_class = AuthorPagination
+    http_method_names = ['get', 'post']
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['name', 'email']  # точное совпадение
+    search_fields = ['name', 'email']  # поиск по части текста
+    ordering_fields = ['name', 'email', 'id']  # сортировка
+
+    @action(detail=True, methods=['post'])
+    def my_action(self, request, pk=None):
+        return Response({'message': f'Пользовательская функция для пользователя с pk={pk}'})
